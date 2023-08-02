787c71082a2ffbbcaa0f5f4c6810206ca8374007
Lucas Ramela <lucasramela@hotmail.com>
2023-08-02 14:42:53 -0300

    Webserver listo

---
+++ app-old.js
@@ -0,0 +1,14 @@
+
+    const http = require('http');
+
+    http.createServer( (req, res) => {
+
+        console.log(req);
+        
+        res.write('Hola mundo!');
+        res.end();
+
+    })
+    .listen( 8080 );
+
+    console.log('Escuchando en el puerto', 8080);
---
+++ app.js
@@ -0,0 +1,65 @@
+
+
+const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estático
+app.use( express.static( 'public' ) );
+
+// render view /
+app.get('/', (req, res) => {
+    res.render('home', {
+
+        nombre: 'Fernando Herrera',
+        titulo: 'Curso de Node'
+
+    });
+})
+
+// render view /generic
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+
+        nombre: 'Fernando Herrera',
+        titulo: 'Curso de Node'
+
+    });
+})
+
+// render view /elements
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+
+        nombre: 'Fernando Herrera',
+        titulo: 'Curso de Node'
+
+    });
+})
+
+// app.get('/elements', (req, res) => {
+//     res.sendFile(__dirname + '/public/elements.html');
+// })
+
+// app.get('/generic', (req, res) => {
+//     res.sendFile(__dirname + '/public/generic.html');
+// })
+
+app.listen( port, () => { 
+
+    console.log(`Example app listering at http://localhost: ${ port }`);
+
+ })
+
+ // app.get('/hola-mundo', (req, res) => {
+//     res.send('Hola mundo en su respectiva ruta');
+// })
+
+// app.listen(8080)
